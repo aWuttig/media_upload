f475dadedaa6193b36bdc11f4f0fe1ecbb90f273
awuttig <andr.wuttig@gmail.com>
2020-06-25 22:03:16 +0200

    [TASK] adds dependency for typo3 v10 to ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '9.5.0-9.5.99',
+                    'typo3' => '10.0.0-10.99.99',
                 ],
             'conflicts' =>
                 [
